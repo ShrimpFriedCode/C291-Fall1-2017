213b25ee54675747dd05440d8d4bb5143774a834
Ethan Anderson <etmander@indiana.edu>
2017-08-23 17:53:51 -0400

    code for ex1

--- exercise/wk1/hello.c
+++ exercise/wk1/hello.c
@@ -2,15 +2,22 @@
  *
  * Filename: main.c
  * Description: Basic C program to print out hello world! 
- * Author: Adeel Bhutta
+ * Author: Adeel Bhutta, Ethan Anderson
  *
 */
 
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void) {
-  printf("Hello World!\n");
-  return(0); 
+
+  printf("Hello World!\n"); 
+  int pos = 0;
+  while(10 > pos){
+    sleep(1);
+    printf(".\n");
 }
 
+return(0);
 
+}
