f122a729d6a1062c5768a01602d9e085f16cbedc
Ethan Anderson <etmander@indiana.edu>
2017-08-31 17:12:01 -0400

    done with quiz

--- exercise/wk2/file1.c
+++ exercise/wk2/file1.c
@@ -1,11 +1,14 @@
 /*Debugging quiz - File No: 1 */
 /* The program should print the sum of all even numbers between 1 to 100 */
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main(void){
-	int limit = 100, sum;
-	for(int counter = 2; counter < limit; counter = counter+2){
+	int limit = 100, sum = 0;
+	int counter;
+	for(counter = 0; counter <= limit; counter+=2){
 		sum = sum+counter;
-	printf("Sum = %d\n", counter);
 	}
+	printf("Sum = %d\n", sum);
+	return 0;
+}
--- exercise/wk2/file2.c
+++ exercise/wk2/file2.c
@@ -1,13 +1,14 @@
 /*Debugging quiz - File No: 2 */
 /* The program should correctly print the stored variable values */
 
-include<stdio.h>
+#include <stdio.h>
 
 int main(){
-	int default = 3.14;2b = 9;
+	double de = 3.14;
+	int b = 9;
 	char alphabet = 'd';
-	printf("%Lf\n",&default)
-	printf("%u",&2b)
-	print("The ASCII value of character 'd' is : %d",'alphabet')
+	printf("%f\n",de);
+	printf("%d\n",b);
+	printf("The ASCII value of character 'd' is : %d\n",alphabet);
 	return 0;
 }
