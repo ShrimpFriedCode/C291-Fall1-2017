fafc97c58ba2ef57a158ae4c30ac964726300d66
Ethan Anderson <etmander@indiana.edu>
2017-10-02 21:23:58 -0400

    Milestone 1 complete

--- tetris/tetris.h
+++ tetris/tetris.h
@@ -34,8 +34,8 @@
 
 /* Code: */
 
-#define WELL_WIDTH 25
-#define WELL_HEIGHT 22
+#define WELL_WIDTH 38
+#define WELL_HEIGHT 33
 
 #define BASE_FALL_RATE 500 // Provides a fall velocity of 500mS/row fall rate. 
 #define DROP_RATE 10       // Provided a drop velocity of 10mS/row fall rate for dropping with spacebar.
